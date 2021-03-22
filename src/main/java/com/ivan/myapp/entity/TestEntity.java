554d2f05ad8593c34b5b82fbf141c9cf405adbcd
1vanM0nt13l <pruebasivan10@gmail.com>
2021-03-22 03:37:43 -0600

    Nombre del proyecto: my-app, Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/ivan/myapp/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.ivan.myapp.entity;
+
+/**
+ * @author omar_montiel
+ *
+ */
+public class TestEntity {
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo testentity
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
